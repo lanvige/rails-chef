e1778c266e09893d589161e7c876c754e4391ae9
Lanvige Jiang <lanvige@gmail.com>
2014-06-25 18:48:00 +0800

    update cookbook

--- site-cookbooks/rails_gem_dependencies/recipes/default.rb
+++ site-cookbooks/rails_gem_dependencies/recipes/default.rb
@@ -16,11 +16,14 @@ package 'libmysqlclient-dev'
 config_dir = "/var/apps"
 
 directory config_dir do
-  owner 'ares'
-  group 'ares'
-  mode "0644"
+  owner node[:deploy_user][:name]
+  group node[:deploy_user][:group]
+  mode "0755"
   recursive true
 end
 
-
+# 0751
+# drwxr-xr-x  2 ares ares  4096 Jun 25 16:30 apps/
+# 0644 不能读不能写
+# drw-r--r--  3 ares ares  4096 Jun 25 16:32 apps/
 
